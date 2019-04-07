6e2e34469fb5fcb6f2231842e24ee87dd525728f
Tiago <tiago.souza.santos1989@gmail.com>
2019-04-07 18:42:31 -0300

    dev-prod

--- api/auth/authService.js
+++ api/auth/authService.js
@@ -3,11 +3,19 @@ const axios = require('axios');
 
 async function post(data) {
     try {
-        let auth = await axios.post('https://github.com/login/oauth/access_token', {
-            client_id: 'cc5d6a52910648e97389',
-            client_secret: '1163f8081f7a567b5cd264b2b42394236a293323',
-            code: data.code
-        },{
+        let authData = {
+            "localhost":{
+                client_id: '3a8e081b8960df638bca',
+                client_secret: '6a9ededb76e0ded36e52f392df75272c589c8bc1',
+                code: data.code
+            },
+            "tiagodesouzasantos.github.io":{
+                client_id: 'cc5d6a52910648e97389',
+                client_secret: '1163f8081f7a567b5cd264b2b42394236a293323',
+                code: data.code
+            }
+        }
+        let auth = await axios.post('https://github.com/login/oauth/access_token', authData[data.server] ,{
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
